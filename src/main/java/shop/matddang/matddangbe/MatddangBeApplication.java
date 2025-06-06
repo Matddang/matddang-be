bbbbd33693a71580f8eade07d1ee945a52449fda
Gothax <scott990328@gmail.com>
2025-06-06 11:37:47 +0900

    feat: init project

---
+++ src/main/java/shop/matddang/matddangbe/MatddangBeApplication.java
@@ -0,0 +1,13 @@
+package shop.matddang.matddangbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatddangBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatddangBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/matddang/matddangbe/MatddangBeApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.matddang.matddangbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatddangBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
