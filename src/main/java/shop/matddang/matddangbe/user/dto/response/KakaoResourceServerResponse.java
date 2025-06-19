c229ca9c399f5d120359e29f437c3b3372a654bd
Gothax <scott990328@gmail.com>
2025-06-20 00:30:45 +0900

    fix: 카카오 소셜로그인 예외처리 추가

--- src/main/java/shop/matddang/matddangbe/user/dto/response/KakaoResourceServerResponse.java
+++ src/main/java/shop/matddang/matddangbe/user/dto/response/KakaoResourceServerResponse.java
@@ -1,11 +1,14 @@
 package shop.matddang.matddangbe.user.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 
 public record KakaoResourceServerResponse(
         Long id,
         @JsonProperty("kakao_account")
+        @NotBlank
         KakaoAccount kakaoAccount,
+        @NotBlank
         KakaoProperties properties
 ) {
     public record KakaoAccount(
