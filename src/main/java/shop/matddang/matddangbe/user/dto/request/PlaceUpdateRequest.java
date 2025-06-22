6377596554d82d61eed939a59b7a4d866c19e299
Gothax <scott990328@gmail.com>
2025-06-23 02:11:59 +0900

    docs: 요청 스펙 문서화 추가

--- src/main/java/shop/matddang/matddangbe/user/dto/request/PlaceUpdateRequest.java
+++ src/main/java/shop/matddang/matddangbe/user/dto/request/PlaceUpdateRequest.java
@@ -1,11 +1,16 @@
 package shop.matddang.matddangbe.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import shop.matddang.matddangbe.user.domain.enums.PlaceType;
 
 public record PlaceUpdateRequest(
         @NotNull
+        @Schema(
+                description = "장소 타입",
+                example = "HOME, FARM"
+        )
         PlaceType placeType,
         @NotBlank
         String placeName,
