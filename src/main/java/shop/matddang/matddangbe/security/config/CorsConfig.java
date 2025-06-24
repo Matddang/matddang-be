89f7ba70e151c123e8f856f643c70676cc6c6545
Gothax <scott990328@gmail.com>
2025-06-24 15:00:26 +0900

    fix: patch mapping cors 누락 수정

--- src/main/java/shop/matddang/matddangbe/security/config/CorsConfig.java
+++ src/main/java/shop/matddang/matddangbe/security/config/CorsConfig.java
@@ -1,25 +1,21 @@
 package shop.matddang.matddangbe.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class CorsConfig {
-    // TODO: production 환경에서 설정 변경
+    @Bean
     public UrlBasedCorsConfigurationSource apiCorsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        config.setAllowedHeaders(Arrays.asList(
-                "Authorization",
-                "Content-Type",
-                "Accept",
-                "X-Requested-With",
-                "Cache-Control"
-        ));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
         config.addExposedHeader("Authorization");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
